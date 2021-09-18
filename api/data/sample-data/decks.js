c88c26a258f37b6529d3f55f4a1c615b9ca8b848
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-09-18 16:49:27 -0700

    updates deck colors in seed data

--- api/data/sample-data/decks.js
+++ api/data/sample-data/decks.js
@@ -2,25 +2,25 @@ const decks = [
   { // 1
     deck_name: 'English Nouns',
     deck_description: 'Sample description for English Nouns',
-    deck_color: '#343434',
+    deck_color: '#f5d3a9',
     user_id: 1,
   },
   { // 2
     deck_name: 'English Phrases',
     deck_description: 'Sample description for English Phrases',
-    deck_color: '#343434',
+    deck_color: '#a9f5e8',
     user_id: 1,
   },
   { // 3
     deck_name: 'Spanish Nouns',
     deck_description: 'Sample description for Spanish Nouns',
-    deck_color: '#343434',
+    deck_color: '#bda9f5',
     user_id: 4,
   },
   { // 4
     deck_name: 'Spanish Phrases',
     deck_description: 'Sample description for Phrases',
-    deck_color: '#343434',
+    deck_color: '#f5d3a9',
     user_id: 4,
   },
 ];
