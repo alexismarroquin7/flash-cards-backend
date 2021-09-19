231597ff7cf6c91de7127f79ae90cc4838ecce7b
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-09-18 21:51:44 -0700

    cards - findAll, findById, deleteById

---
+++ api/cards/cards-router.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { validateSortQuery, validateDeckIdQuery, validateCardIdExists } = require('./cards-middleware');
+const Card = require('./cards-model');
+
+router.get('/', validateSortQuery, validateDeckIdQuery, async (req, res, next) => {
+  try {
+    const card = await Card.findAll(req.query);
+    res.status(200).json(card);
+
+  } catch(err){
+    next(err)
+  }
+
+});
+
+router.get('/:card_id', validateCardIdExists, (req, res, next) => {
+  res.status(200).json(req.card);
+});
+
+router.delete('/:card_id', validateCardIdExists, async (req, res, next) => {
+  try {
+    const deletedCard = await Card.deleteById(req.card.card_id);
+    
+    res.status(200).json({
+      card_id: deletedCard.card_id
+    });
+  
+  } catch(err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  });
+});
+
+module.exports = router;
